58979b80848853d3536c4e7400700dfc91a8353b
adithyan <adithyanunair01@gmail.com>
2024-04-22 15:33:31 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { ShopComponent } from './shop/shop.component';
 import { ContactComponent } from './contact/contact.component';
 import { CartComponent } from './cart/cart.component';
 import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { NewsComponent } from './news/news.component';
 
 export const routes: Routes = [
     {path:'', component:HomeComponent},
     {path:'shop', component:ShopComponent},
     {path:'contact', component:ContactComponent},
     {path:'cart', component:CartComponent},
+    {path:'news', component:NewsComponent},
     {path:'user-registration', component:UserRegistrationComponent}
 
 ];
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -13,7 +13,7 @@ export class ShopComponent {
 cartServices = inject(CartService);
 shops:any[]=[
  {name:'Gymnocalcium Cactus',imageUrl:"../assets/image/shop-cactus-1.png", price:15 , id:1},
- {name:'Echeveria al Succulent',imageUrl:"../assets/image/shop-cactus-2.png",price:20, id:2},
+ {name:'Echeveria al Succulent',imageUrl:"../assets/image/shop-cactus-2.png",price:20, id:2}, 
  {name:'Ferrocacneas Cactus',imageUrl:"../assets/image/shop-cactus-3.png",price:23, id:3},
  {name:'Key Lime Pie Succulent',imageUrl:"../assets/image/shop-cactus-4.png",price:43, id:4},
  {name:'Melocacneas Cactus',imageUrl:"../assets/image/shop-cactus-5.png",price:23, id:5},
